321d05c1a03e731c34f12a15efe555295cd4bd05
Milan Nariya <milann.wedowebapps@gmail.com>
2024-07-30 12:37:54 +0530

    first commit

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+// import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+export default function Github() {
+  //   const [githubData, setGithubData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/hiteshchoudhary").then((res) =>
+  //       res.json().then((data) => {
+  //         console.log(data);
+  //         setGithubData(data);
+  //       })
+  //     );
+  //   }, []);
+
+  const githubData = useLoaderData();
+  return (
+    <div className="text-center m-4 bg-gray-500 text-white text-3xl p-4">
+      Github Followers : {githubData.followers}
+      <img src={githubData.avatar_url} alt="Git pic" width={300} />
+    </div>
+  );
+}
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/mils7023");
+  return response.json();
+};
