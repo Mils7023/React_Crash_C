4e2c32a7fe8f0d1f687e00150a09c5510d142d4e
Milan Nariya <milann.wedowebapps@gmail.com>
2024-07-30 16:56:01 +0530

    Added theme switcher

---
+++ themeSwitcher/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { TheameProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    const element = document.querySelector("html").classList;
+    element.remove("light", "dark");
+    element.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <TheameProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </TheameProvider>
+  );
+}
+
+export default App;
